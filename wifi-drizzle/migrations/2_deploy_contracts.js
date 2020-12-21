b25080ed3f084d38dd7f9ef1f4beb5f5aa025861
Aaron <Airine0119@gmail.com>
2020-12-21 18:55:47 +0800

    update ip and owner account address

--- wifi-drizzle/migrations/2_deploy_contracts.js
+++ wifi-drizzle/migrations/2_deploy_contracts.js
@@ -3,8 +3,8 @@ const WifiAllocation = artifacts.require("WifiAllocation");
 module.exports = function(deployer) {
     // _totalBandwidth: 50, _minPrice: 1, _burstPrice: 1
     
-    deployer.deploy(WifiAllocation, 30, 1, 1, {
-        from: '0x0cbbb79b02449ea575f6185dd3c541e9ab8d8182',
+    deployer.deploy(WifiAllocation, 100, 1, 1, {
+        from: '0x0671a40872727Ff3c610DD287e88c5b3672A6b30',
         // from: '0xBfe977EfAc7e414A7a441f3be640aa42ed514093',
         overwrite: true
     });
